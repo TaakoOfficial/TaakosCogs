f2b236499f25940b3b08e1819c9ece629aff1586
Taako <cm312k9@gmail.com>
2025-04-13 02:14:39 -0500

    Refactor WeatherCog: rename module and consolidate implementation into a single file

--- RandomWeather/__init__.py
+++ RandomWeather/__init__.py
@@ -1,4 +1,4 @@
-from .random_weather import WeatherCog  # Edited by Taako
+from .randomweather import WeatherCog  # Edited by Taako
 
 async def setup(bot):
     await bot.add_cog(WeatherCog(bot))  # Edited by Taako
